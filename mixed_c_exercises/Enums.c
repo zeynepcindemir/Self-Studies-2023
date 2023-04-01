2d9f82353c375f72aa912c4e8f7af4586d68a5fc
zeynepcindemir <98207485+zeynepcindemir@users.noreply.github.com>
2023-04-01 16:46:47 +0300

    Create Enums.c

---
+++ mixed_c_exercises/Enums.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+enum months {Jan = 1, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec};
+
+int main(int argc, char const *argv[])
+{
+    const char *monthName[] = {"", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+  
+    for(enum months month = Jan; month <= Dec; month++)
+        printf("%2d%11s\n", month, monthName[month]);
+  
+    return 0;
+}
