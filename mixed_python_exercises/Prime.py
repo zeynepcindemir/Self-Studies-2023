bfe29556aaea7f6d0448c7cad1f0ef2bd64d80fa
zeynepcindemir <98207485+zeynepcindemir@users.noreply.github.com>
2023-04-01 16:55:12 +0300

    Create Prime.py

---
+++ mixed_python_exercises/Prime.py
@@ -0,0 +1,15 @@
+def is_prime(number):
+   
+    if number < 2:
+        return False
+    for i in range(2, int(number ** 0.5) + 1):
+        if number % i == 0:
+            return False
+    return True
+
+
+num = 17
+if is_prime(num):
+    print(num, "is a prime number")
+else:
+    print(num, "is not a prime number")
